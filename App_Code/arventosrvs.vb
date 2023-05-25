4f0bbaf2d47323359e8103f3dd6fb9d02d4fd1ee
cuneytsener <cuneytsener53@gmail.com>
2023-05-25 12:21:30 +0300

    first commit

---
+++ App_Code/arventosrvs.vb
@@ -0,0 +1,50 @@
+﻿Imports System.Data
+Imports ArventoSRV
+Imports Microsoft.VisualBasic
+
+Public Class arventosrvs
+
+    Shared client = New ArventoReportWebService 'SERVİS BAĞLANTISI İÇİN GEREKLİ KOD'
+
+    'DEĞİŞKENLERİ TANIMLIYORUZ'
+    Shared username = ""  '
+    Shared pin1 = "."
+    Shared pin2 = "."
+
+    Public Shared Function GetDeviceId(ByVal newPlate As String) As String 'GELEN DÜZENLENMİŞ PLAKA İLE SERVİSİ KULLANARAK PLAKA DEVİCEİD BİLGİSİNİ ALIR'
+        Dim group = ""
+        Dim dateset = client.GetNodes(username, pin1, pin2, group)
+        Dim query = "LicensePlate like '%" + newPlate + "%'"
+        Dim datarow = dateset.Tables(0).Select(query)
+        Dim deviceId = datarow(0).ItemArray(1).ToString
+
+        Return deviceId
+    End Function
+
+    Public Shared Function Location(ByVal plate As String) As String 'BU FONKSİYON ARACIN LOCATİON BİLGİSİNİ VERİYOR'
+        Dim newPlate = EditPlate(plate) 'GELEN PLAKAYI DÜZENLİYORUZ'
+        Dim deviceId = GetDeviceId(newPlate) 'PLAKAYA AİT DeviceId'Yİ ALIYORUZ'
+        Dim language = "1"
+        Dim dateset = client.GetVehicleStatusByNode(username, pin1, pin2, deviceId, language)
+        Dim datarow = dateset.Tables(0).Rows
+        Dim locationInfo = "" 'HER PLAKA İÇİN LOCATİON BİLGİSİ ALINACAĞINDAN BAŞTA LOCATİON BİLGİSİNİ TEMİZLİYORUZ'
+
+        For Each item As String In datarow(0).ItemArray
+
+            'SERVİSTEN GELEN LOCATİONBİLGİSİNİN BELLİ DEĞERLERİNİ ALACAĞIMIZ İÇİN BURADA BU KODU TANIMLADIK'
+            'locationInfo = item.ItemArray(0).ToString + "|" + item.ItemArray(2).ToString + "|" + item.ItemArray(3).ToString + "|" + item.ItemArray.ToString(5)
+
+
+            locationInfo += item + " | " 'BURADA SERVİSTEN GELEN TÜM LOKASYON BİLGİSİNİ TEK SATIR OLARAK ALDIK'
+        Next
+
+        Return locationInfo 'BURADA LOKASYON BİLGİSİNİ GERİ DÖNDERDİK'
+    End Function
+
+    Public Shared Function EditPlate(ByVal plate As String) As String
+        Dim newPlate = Regex.Replace(plate.Replace(" ", ""), "(\d+)(\D+)(\d+)", "$1 $2 $3") 'BURADA BOŞ GELEN PLAKALARI YADA YALNIŞ YAZILMIŞ PLAKALARI REGEX KODU İLE DÜZELTTİK'
+
+        Return newPlate
+    End Function
+
+End Class
---
+++ Default.aspx.vb
@@ -0,0 +1,20 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Dim plate = Request.QueryString("plate") 'BU KODDA İSE QUERYSTRİNG KULLANARAK TARAYICIDAKİ LİNK KISMINDA YAZILAN DEĞERLERİ ALIR'
+
+        If IsNothing(plate) Then 'EĞER GELEN PLAKA BİLGİSİ YADA DEĞER BOŞ İSE BU İF KODU İLE RETURN EDEREK SERVİSİ YORMUYORUZ'
+            Return
+        End If
+
+        Dim locationInfo = arventosrvs.Location(plate) 'BU KOD PLAKANIN LOCATİON BİLGİSİ İÇİN METHODU ÇAĞIRIYORUZ'
+
+        Response.Write(locationInfo) 'BU KOD LOCATION BİLGİSİNİ EKRANA YAZDIRIR'
+
+    End Sub
+
+
+End Class
